224ebeb987cebe2a26e348f07e08c3d1f00c5b0c
Sonu Tiwari <ankorha@gmail.com>
2020-07-18 12:36:21 +0530

    Add Comments

--- index.js
+++ index.js
@@ -5,19 +5,30 @@ const reader = fs.createReadStream('./sample.csv', {
   encoding: 'utf8',
 });
 
-const JsonData = [];
+const jsonData = [];
 
+/**
+ * The function will parse one chunk of csv string to corresponding JSON.
+ * @param {string} chunk Current csv string to be processed
+ * @param {string} header Header for the current csv strings(Work as keys)
+ */
 function parseCSVChunk(chunk, header) {
-  let lines = chunk.split(/\r|\n|\r\n/g);
-  lines = lines.filter((ele) => {
-    return /\S/.test(ele);
-  });
+  // Remove line feeds from chunk.
+  let lines = chunk.split(/\r|\n|\r\n|\r?\n/g);
+  // check for valid line.
+  lines = lines.filter((ele) => /\S/.test(ele));
+  // Create key value pair if header is valid
   if (header) {
     const keys = lines.shift();
     generateJSON(lines, keys.split(','));
-  } else generateJSON(lines);
+  } else generateJSON(lines); // Else simply create json without keys.
 }
 
+/**
+ * Generate JSON from objects and keys.
+ * @param {Array.<string>} lines Array of JSON Objects.
+ * @param {Array.<string>} keys Array of JSON keys.
+ */
 function generateJSON(lines, keys) {
   let i = 0;
   for (const line of lines) {
@@ -36,32 +47,42 @@ function generateJSON(lines, keys) {
         object = stringArray;
       }
     }
-    JsonData.push(object);
+    jsonData.push(object);
   }
 }
 
-class createObjectStream extends Readable {
-  constructor(Array) {
+/**
+ * Class to read csv as stream and parse it.
+ */
+class CreateObjectStream extends Readable {
+  constructor(readableStreamArray) {
     super({ objectMode: true });
-    this.Array = Array;
+    this.readableStreamArray = readableStreamArray;
     this.index = 0;
   }
+
+  /**
+   * overiding read method of Readable class.
+   */
   _read() {
-    if (this.index < this.Array.length) {
-      this.push(this.Array[this.index++]);
+    if (this.index < this.readableStreamArray.length) {
+      this.push(this.readableStreamArray[this.index++]);
     } else {
       this.push(null);
     }
   }
 }
 
+/**
+ * Parse the Readable stream asynchronously.
+ */
 const parse = async () => {
   let header = false;
   for await (const chunk of reader) {
     parseCSVChunk(chunk, header);
     header = false;
   }
-  return new createObjectStream(JsonData);
+  return new CreateObjectStream(jsonData);
 };
 
 module.exports.parse = parse;
